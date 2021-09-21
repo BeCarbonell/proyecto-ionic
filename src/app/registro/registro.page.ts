6b152727ec95e2a3773d55440f55680c29f3c218
BeCarbonell <be.carbonell@duocuc.cl>
2021-09-21 01:10:22 -0300

    Registro actualizado mas ts

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UsuariosService } from '../usuarios.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  constructor(private usuariosService: UsuariosService,
+              private router : Router
+    ) { }
 
   ngOnInit() {
   }
-
+  agregar(nombre :HTMLInputElement, apellido:HTMLInputElement, nomUsuario:HTMLInputElement, contrasena:HTMLInputElement)
+  {
+    const nom = nombre.value;
+    const ape = apellido.value;
+    const usu = nomUsuario.value;
+    const cont = contrasena.value;
+    this.usuariosService.addUsuario(nom, ape, usu, cont);
+    this.router.navigate(['/'])
+  }
 }
